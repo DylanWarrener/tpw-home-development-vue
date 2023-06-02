610c0a03f9917dd8db95b8a75bab2a529ed699c2
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-02 15:28:21 +0100

    Seperated vuetify and vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createVuetify } from "vuetify";
-import { createI18n } from "vue-i18n";
-
-// Vuetify styles
-import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
-
-// Vuetify icons
-import { aliases, mdi } from "vuetify/iconsets/mdi";
-
-// Vuetify components & directives
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 // Localisation
-//import en from "./locales/en.json";
-import en from "./locales/en.js";
+import vuei18n from "./vue-i18n.ts";
+
+// Vuetify
+import vuetify from "./vuetify.ts";
 
 // Routes
 import router from "./router";
@@ -26,22 +15,9 @@ import App from "./app.vue";
 
 const app = createApp(App);
 const pinia = createPinia();
-const i18n = createI18n({
-	locale: "en",
-	messages: { en },
-});
-const vuetify = createVuetify({
-	icons: {
-		defaultSet: "mdi",
-		aliases,
-		sets: { mdi },
-	},
-	components,
-	directives,
-});
 
 app.use(router);
 app.use(pinia);
 app.use(vuetify);
-app.use(i18n);
+app.use(vuei18n);
 app.mount("#app");
