2f29ca667019db193f6ee3e5b6d45ba414101129
DylanWarrener <dqyqlqaqn@gmail.com>
2023-06-03 23:48:39 +0100

    Added app content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
 	<v-layout>
-		<header-component></header-component>
+		<!--<v-system-bar color="blue"></v-system-bar>-->
+		<v-app-bar 
+			prominent
+			color="primary" 
+			density="compact"
+			scroll-behavior="hide elevate">
+
+			<template v-slot:prepend>
+				<v-app-bar-nav-icon
+					variant="text"
+					@click.stop="navMenuClicked">
+				</v-app-bar-nav-icon>
+			</template>
+
+			<v-app-bar-title>{{ $t("welcome") }}</v-app-bar-title>
+			
+			<template v-slot:append>
+				<v-btn icon="mdi-dots-vertical"></v-btn>
+			</template>
+		</v-app-bar>
+		<header-component :drawer="drawer"></header-component>
 		<v-main>
 			<router-view></router-view>
 		</v-main>
@@ -11,6 +31,9 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+// Interfaces
+import ICommonData from "./interfaces/common/interface-common";
+
 import Header from "./components/header/header.vue";
 import Footer from "./components/footer/footer.vue";
 
@@ -19,6 +42,16 @@ export default defineComponent({
 	components: {
 		"header-component": Header,
 		"footer-component": Footer,
+	},
+	methods: {
+		navMenuClicked(): void {
+			this.drawer = !this.drawer;
+		}
+	},
+	data(): ICommonData {
+		return {
+			drawer: false,
+		}
 	},
 });
 </script>
