a1cb7d1f8294ecec10d3d9d2b1a453bfc8a6458d
DylanWarrener <dqyqlqaqn@gmail.com>
2023-06-03 23:47:52 +0100

    Added navigation

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,5 +1,23 @@
 <template>
-	<v-app-bar>{{ $t("welcome") }}</v-app-bar>
+	<v-navigation-drawer 
+		v-model="drawer"
+		location="top"
+		temporary>
+		<v-list nav>
+			<v-list-item 
+				to="/"
+				prepend-icon="mdi-home"
+				title="Home"
+				value="home">
+			</v-list-item>
+			<v-list-item 
+				to="/kitchens"
+				prepend-icon="mdi-kitchen"
+				title="Kitchens"
+				value="kitchens">
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
 </template>
 
 <script lang="ts">
@@ -7,5 +25,17 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
 	name: "header-component",
+	props: ['drawer'],
+	data() {
+		return {
+			group: null,
+			items: [
+				{ title: 'Foo', value: 'foo' },
+				{ title: 'Bar', value: 'bar' },
+				{ title: 'Fizz', value: 'fizz' },
+				{ title: 'Buzz', value: 'buzz' },
+			],
+		}
+	}
 });
 </script>
