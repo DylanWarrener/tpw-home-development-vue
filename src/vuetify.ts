b406273379f714ea0140327baf974da3eaec8e66
DylanWarrener <dqyqlqaqn@gmail.com>
2023-06-03 23:48:31 +0100

    Added icons, styles and components

--- src/vuetify.ts
+++ src/vuetify.ts
@@ -1,43 +1,64 @@
 // General import
 import { createVuetify } from "vuetify";
-import { VLayout, VMain, VAppBar, VFooter } from "vuetify/components";
 
 // Styles
 //import "vuetify/styles";
 import "vuetify/dist/vuetify.min.css";
-//import "@mdi/font/css/materialdesignicons.min.css";
+import "@mdi/font/css/materialdesignicons.min.css";
 
 // Icons
-//import { aliases, mdi } from "vuetify/iconsets/mdi";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 // Components
-//import { VLayout, VAppBar, VMain, VTextField, VBtn, VCard, VFooter } from "vuetify/components";
+import { 
+	VLayout, 
+	VSystemBar,
+	VAppBar, 
+	VAppBarNavIcon,
+	VAppBarTitle,
+	VBtn, 
+	VNavigationDrawer,
+	VList,
+	VListItem,
+	VMain, 
+	VContainer,
+	VRow,
+	VCol,
+	VFooter 
+} from "vuetify/components";
 
 // Directives
-//import { Resize, Ripple } from "vuetify/directives";
+import { Resize, Ripple } from "vuetify/directives";
 
 //import * as components from "vuetify/components";
 //import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
-	/* icons: {
-		iconfont: "mdi",
-		defaultSet: "mdi",
-		aliases,
-		sets: { mdi },
-	}, */
-	/* components,
-	directives, */
-	components: {
-		VLayout,
-		VMain,
-		VAppBar,
-		VFooter,
-	},
-	/* directives: {
-		Resize,
-		Ripple,
-	}, */
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: { mdi },
+  },
+  components: {
+    VLayout,
+    VSystemBar,
+    VAppBar,
+    VAppBarNavIcon,
+    VAppBarTitle,
+    VBtn,
+    VNavigationDrawer,
+    VList,
+    VListItem,
+    VMain,
+    VContainer,
+    VRow,
+    VCol,
+    VFooter,
+  },
+  directives: {
+    Resize,
+    Ripple,
+  },
 });
 
 export default vuetify;
