6170d6d0440538b6f7a2bfdeaaf58220825f3c07
DylanWarrener <dqyqlqaqn@gmail.com>
2023-06-04 09:53:58 +0100

    Removed navigation drawer from header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,23 +1,5 @@
 <template>
-	<v-navigation-drawer 
-		v-model="drawer"
-		location="top"
-		temporary>
-		<v-list nav>
-			<v-list-item 
-				to="/"
-				prepend-icon="mdi-home"
-				title="Home"
-				value="home">
-			</v-list-item>
-			<v-list-item 
-				to="/kitchens"
-				prepend-icon="mdi-kitchen"
-				title="Kitchens"
-				value="kitchens">
-			</v-list-item>
-		</v-list>
-	</v-navigation-drawer>
+	<div>Header</div>
 </template>
 
 <script lang="ts">
@@ -25,17 +7,6 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
 	name: "header-component",
-	props: ['drawer'],
-	data() {
-		return {
-			group: null,
-			items: [
-				{ title: 'Foo', value: 'foo' },
-				{ title: 'Bar', value: 'bar' },
-				{ title: 'Fizz', value: 'fizz' },
-				{ title: 'Buzz', value: 'buzz' },
-			],
-		}
-	}
+	props: ['drawer']
 });
 </script>
