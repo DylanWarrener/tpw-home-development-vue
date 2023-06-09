16404daae7cc427c3d955fa2381e119ebe6b4e40
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-09 15:54:43 +0100

    Added text to locale

---
+++ src/locales/en.ts
@@ -0,0 +1,59 @@
+import ITextualData from "../interfaces/common/locale/interface-locale";
+
+const en: ITextualData = {
+	common: {},
+	header: {
+		appBar: {
+			title: "Home",
+			icons: {
+				verticalDots: "mdi-dots-vertical",
+			},
+			text: {
+				home: "Home",
+				kitchen: "Kitchens",
+				bathroom: "Bathrooms",
+				newbuild: "Newbuilds",
+				extension: "Extensions",
+				refurbishment: "Refurbishments",
+				contact: "Contact",
+				about: "About",
+			},
+		},
+		navigation: {
+			text: {
+				home: "Home",
+				kitchen: "Kitchens",
+				bathroom: "Bathrooms",
+				newbuild: "Newbuilds",
+				extension: "Extensions",
+				refurbishment: "Refurbishments",
+				contact: "Contact",
+				about: "About",
+			},
+			links: {
+				home: "/",
+				kitchen: "/kitchens",
+				bathroom: "/bathrooms",
+				newbuild: "/newbuilds",
+				extension: "/extensions",
+				refurbishment: "/refurbishments",
+				contact: "/contact",
+				about: "/about",
+			},
+			icons: {
+				home: "mdi-home",
+				kitchen: "mdi-faucet",
+				bathroom: "mdi-shower-head",
+				newbuild: "mdi-office-building-plus",
+				extension: "mdi-toy-brick-plus",
+				refurbishment: "mdi-brush",
+				contact: "mdi-email-fast",
+				about: "mdi-information",
+			},
+		},
+	},
+	main: {},
+	footer: {},
+};
+
+export default en;
