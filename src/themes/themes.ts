453080258cb4eed3d094ef8862290b58e2f1d3d9
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-22 18:11:36 +0100

    Updated themes for vuetify

--- src/themes/themes.ts
+++ src/themes/themes.ts
@@ -1,41 +1,74 @@
-import { ThemeDefinition } from "vuetify";
+// Interfaces
+import ICommonThemes from "../interfaces/common/theme/interface-common-theme";
 
-const themeLight: ThemeDefinition = {
+const themeLight: ICommonThemes = {
 	dark: false,
 	colors: {
+		default: "",
+		primary: "#3d3a4f",
+		secondary: "#777494",
+		accent: "#7f017f",
+		white: "#FFFFFF",
+		black: "#000000",
+		yellow: "#FFFF00",
+		red: "#FF0000",
+		green: "#00FF00",
+		blue: "#0000FF",
+		background: "",
+		surface: "",
+		button: "",
+		error: "",
+		warning: "",
+		info: "",
+		success: "",
+		"section-primary": "",
+		"section-secondary": "",
+	},
+};
+
+export const themeDark: ICommonThemes = {
+	dark: true,
+	colors: {
+		default: "",
 		primary: "#777494",
 		secondary: "#3d3a4f",
 		accent: "#7f017f",
 		white: "#FFFFFF",
 		black: "#000000",
+		yellow: "#FFFF00",
+		red: "#FF0000",
+		green: "#00FF00",
+		blue: "#0000FF",
 		background: "",
 		surface: "",
-		error: "#B00020",
+		button: "",
+		error: "",
+		warning: "",
 		info: "",
 		success: "",
-		warning: "#FF0000",
-		"primary-darken-1": "#3700B3",
-		"secondary-darken-1": "#018786",
+		"section-primary": "",
+		"section-secondary": "",
 	},
 };
 
-export const themeDark: ThemeDefinition = {
-	dark: true,
-	colors: {
-		primary: "#3d3a4f",
-		secondary: "#777494",
-		accent: "#6813A4",
-		white: "#FFFFFF",
-		black: "#000000",
-		background: "",
-		surface: "",
-		error: "#B00020",
-		info: "",
-		success: "",
-		warning: "#FF0000",
-		"primary-darken-1": "#3700B3",
-		"secondary-darken-1": "#018786",
-	},
-};
+// Set the values of colors for light theme
+themeLight.colors.default = themeLight.colors.white;
+themeLight.colors.error = themeLight.colors.red;
+themeLight.colors.warning = themeLight.colors.yellow;
+themeLight.colors.info = themeLight.colors.yellow;
+themeLight.colors.success = themeLight.colors.green;
+themeLight.colors.button = themeLight.colors.black;
+themeLight.colors["section-primary"] = themeLight.colors.white;
+themeLight.colors["section-secondary"] = themeLight.colors.secondary;
+
+// Set the values of colors for dark theme
+themeDark.colors.default = themeDark.colors.black;
+themeDark.colors.error = themeDark.colors.red;
+themeDark.colors.warning = themeDark.colors.yellow;
+themeDark.colors.info = themeDark.colors.yellow;
+themeDark.colors.success = themeDark.colors.green;
+themeDark.colors.button = themeDark.colors.white;
+themeDark.colors["section-primary"] = themeDark.colors.black;
+themeDark.colors["section-secondary"] = themeDark.colors.secondary;
 
 export default themeLight;
