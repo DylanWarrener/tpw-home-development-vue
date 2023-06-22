ba0f768677326fda537ff8ef8ead9a9b88e63e3d
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-22 18:13:36 +0100

    Added custom button component

---
+++ src/components/containers/buttons/button.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-btn :stacked="stacked" class="bg-default text-button" :icon="icon" :text="text" rounded="xl"></v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { VBtn } from "vuetify/lib/components/index.mjs";
+
+export default defineComponent({
+	name: "button-component",
+	props: {
+		stacked: { type: Boolean, required: false },
+		//class: { type: String, required: false },
+		//color: { type: String, required: false },
+		icon: { type: String, required: false },
+		text: { type: String, required: false },
+	},
+});
+</script>
