52fadad87b8b2b23d28cfaa301cf245ea3b72b90
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-22 18:16:07 +0100

    Updated locale textual data

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,7 +1,7 @@
 import ITextualData from "../interfaces/common/locale/interface-locale";
 
 // Common page name data
-import { btnCardText, pageNameData } from "../utils/utils-common-text";
+import { btnCardText, pageName } from "../utils/utils-common-text";
 
 const en: ITextualData = {
 	common: {
@@ -11,23 +11,31 @@ const en: ITextualData = {
 		dropdown: {
 			carousel: {
 				items: {
-					kitchen: pageNameData.kitchen,
-					bathroom: pageNameData.bathroom,
-					newbuild: pageNameData.newbuild,
-					extension: pageNameData.extension,
-					refurbishment: pageNameData.refurbishment,
+					kitchen: pageName.kitchen,
+					bathroom: pageName.bathroom,
+					newbuild: pageName.newbuild,
+					extension: pageName.extension,
+					refurbishment: pageName.refurbishment,
 				},
 				label: "Choose environment",
 			},
 		},
 		icon: {},
 		page: {
-			name: pageNameData,
+			name: pageName,
 		},
 		section: {
 			beInspired: {
 				title: "Be Inspired",
-				subtitle: "See what our most popular options are.",
+				subtitle: "See our most popular designs.",
+				style: {
+					title: "Styles",
+					subtitle: "See our most popular styles.",
+				},
+				theme: {
+					title: "Themes",
+					subtitle: "See our most popular themes.",
+				},
 			},
 			portfolio: {
 				title: "Portfolio",
@@ -53,12 +61,12 @@ const en: ITextualData = {
 				verticalDots: "mdi-dots-vertical",
 			},
 			page: {
-				name: pageNameData,
+				name: pageName,
 			},
 		},
 		navigation: {
 			page: {
-				name: pageNameData,
+				name: pageName,
 				link: {
 					home: "/",
 					kitchen: "/kitchens",
@@ -91,37 +99,37 @@ const en: ITextualData = {
 					btnText: btnCardText,
 				},
 				kitchen: {
-					title: pageNameData.kitchen,
+					title: pageName.kitchen,
 					subtitle: "Hash your recipies with our interactive entities.",
 					btnText: btnCardText,
 				},
 				bathroom: {
-					title: pageNameData.bathroom,
+					title: pageName.bathroom,
 					subtitle: "Own your restroom with our pristine collections.",
 					btnText: btnCardText,
 				},
 				newbuild: {
-					title: pageNameData.newbuild,
+					title: pageName.newbuild,
 					subtitle: "Structure new homes with our hasty reinforced foundations.",
 					btnText: btnCardText,
 				},
 				extension: {
-					title: pageNameData.extension,
+					title: pageName.extension,
 					subtitle: "Mammoth your space with our innovative styles.",
 					btnText: btnCardText,
 				},
 				refurbishment: {
-					title: pageNameData.refurbishment,
+					title: pageName.refurbishment,
 					subtitle: "Enlighten and re-establish your home with our ingenuious designs.",
 					btnText: btnCardText,
 				},
 				contact: {
-					title: pageNameData.contact,
+					title: pageName.contact,
 					subtitle: "",
 					btnText: "Contact Us?",
 				},
 				about: {
-					title: pageNameData.about,
+					title: pageName.about,
 					subtitle: "",
 					btnText: "See Team?",
 				},
