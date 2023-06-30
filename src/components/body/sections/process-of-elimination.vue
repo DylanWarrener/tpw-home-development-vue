7cc202e623e16481f6634bb6662f991b1e95de08
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-30 13:25:35 +0100

    Updated title & content

--- src/components/body/sections/process-of-elimination.vue
+++ src/components/body/sections/process-of-elimination.vue
@@ -1,44 +1,60 @@
 <template>
 	<section-component class="bg-section-secondary" :title="title" :subtitle="subtitle">
 		<template #content>
-			<div class="d-flex flex-column flex-1-0">
+			<div class="d-flex flex-column flex-1-0" style="border: 2px solid red">
 				<!-- Discovery -->
-				<div class="d-flex flex-row" style="gap: 16px; min-height: 2000px">
-					<div class="flex-0-1">
-						<h3 class="text-uppercase text-vertical" style="padding: 0 60px 0 0">Discovery</h3>
+				<div class="d-flex flex-row sticky-row">
+					<div class="flex-0-1 sticky-column">
+						<div class="sticky-column-height">
+							<div class="sticky-container">
+								<h3 class="text-uppercase text-vertical">Discovery</h3>
+							</div>
+						</div>
 					</div>
-					<div class="d-flex flex-row flex-1-1">Column 2</div>
+					<div class="d-flex flex-row flex-1-0">Column 2</div>
 				</div>
-				<v-divider class="pa-4"></v-divider>
 
 				<!-- Strategy -->
-				<div class="d-flex flex-row">Strategy</div>
+				<div class="d-flex flex-row sticky-row">
+					<div class="flex-0-1 sticky-column">
+						<div class="sticky-column-height">
+							<div class="sticky-container">
+								<h3 class="text-uppercase text-vertical">Strategy</h3>
+							</div>
+						</div>
+					</div>
+					<div class="d-flex flex-row flex-1-0">Column 2</div>
+				</div>
 
 				<!-- Design -->
-				<div class="d-flex flex-row">Design</div>
+				<div class="d-flex flex-row sticky-row">
+					<div class="flex-0-1 sticky-column">
+						<div class="sticky-column-height">
+							<div class="sticky-container">
+								<h3 class="text-uppercase text-vertical">Design</h3>
+							</div>
+						</div>
+					</div>
+					<div class="d-flex flex-row flex-1-0">Column 2</div>
+				</div>
 
 				<!-- Build -->
-				<div class="d-flex flex-row">Build</div>
+				<div class="d-flex flex-row sticky-row">
+					<div class="flex-0-1 sticky-column">
+						<div class="sticky-column-height">
+							<div class="sticky-container">
+								<h3 class="text-uppercase text-vertical">Build</h3>
+							</div>
+						</div>
+					</div>
+					<div class="d-flex flex-row flex-1-0">Column 2</div>
+				</div>
 			</div>
 		</template>
 	</section-component>
 </template>
 
 <script lang="ts">
-/*
-<div class="w-100 d-flex flex-column" style="min-height: 2000px">
-				<!-- Discovery -->
-				<div class="d-flex flex-row flex-1-0">
-					<div class="pb-12 flex-0-1">
-						<div class="px-14 text-uppercase text-vertical">Discovery</div>
-					</div>
-					<div class="d-flex flex-column flex-1-1">
-						<p>Content test 1</p>
-						<p>Content test 1</p>
-					</div>
-				</div>
-			</div>
-*/
 import { defineComponent } from "vue";
 
 // Components
@@ -61,10 +77,41 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.text-vertical {
+.sticky-row {
+	border: 2px solid black;
+	min-height: 2000px;
+}
+
+.sticky-column {
+	border: 2px solid blue;
+	position: relative;
+	min-width: 50px;
+	padding-right: 100px;
+}
+
+.sticky-column-height {
+	border: 2px solid orange;
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 95%;
+	width: 100%;
+}
+
+.sticky-container {
+	border: 2px solid black;
 	position: sticky;
 	top: 0;
 	left: 0;
-	transform: rotate(-90deg) translate(-50%);
+	padding-top: 100px;
+}
+
+.text-vertical {
+	border: 2px solid green;
+	position: sticky;
+	top: 0;
+	left: 0;
+	transform-origin: top right;
+	transform: translate(-50%, 0) rotate(-90deg);
 }
 </style>
