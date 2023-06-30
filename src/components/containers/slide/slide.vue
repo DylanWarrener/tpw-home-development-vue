15d60ab5ae99efc1a989e4b73433588249e2d151
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-06-30 16:07:06 +0100

    Updated slide component

--- src/components/containers/slide/slide.vue
+++ src/components/containers/slide/slide.vue
@@ -1,18 +1,23 @@
 <template>
 	<div class="d-flex flex-row sticky-row">
 		<!-- Slide title -->
-		<div class="flex-0-1 sticky-column">
+		<div class="sticky-column">
 			<div class="sticky-column-height">
 				<div class="sticky-container">
 					<slot name="title">
-						<h3 class="text-uppercase text-vertical">{{ title }}</h3>
+						<h3 class="text-uppercase text-vertical" style="border: 2px solid green">
+							<b class="pr-4" style="font-size: 30px">{{ slideNumber }}.</b>{{ title }}
+						</h3>
 					</slot>
 				</div>
 			</div>
 		</div>
 
 		<!-- Slide content -->
-		<div class="d-flex flex-row flex-1-0">
+		<div
+			class="pr-4 py-4 d-flex flex-column flex-1-1 justify-center align-left overflow-hidden"
+			style="padding-left: 25%; gap: 16px; border: 2px solid white"
+		>
 			<slot name="content"></slot>
 		</div>
 	</div>
@@ -25,14 +30,15 @@ export default defineComponent({
 	name: "slide-component",
 	props: {
 		title: { type: String, required: false },
+		slideNumber: { type: String, required: false },
 	},
 });
 </script>
 
 <style scoped>
 .sticky-row {
-	border: 2px solid black;
-	min-height: 2000px;
+	border: 2px solid green;
+	min-height: 600px;
 }
 
 .sticky-column {
@@ -43,18 +49,20 @@ export default defineComponent({
 }
 
 .sticky-column-height {
+	border: 2px solid orange;
 	position: absolute;
 	top: 0;
 	left: 0;
-	height: 95%;
+	height: 90%;
 	width: 100%;
 }
 
 .sticky-container {
+	border: 2px solid black;
 	position: sticky;
 	top: 0;
 	left: 0;
-	padding-top: 100px;
+	padding-top: 150px;
 }
 
 .text-vertical {
@@ -63,5 +71,6 @@ export default defineComponent({
 	left: 0;
 	transform-origin: top right;
 	transform: translate(-50%, 0) rotate(-90deg);
+	letter-spacing: 2px;
 }
 </style>
