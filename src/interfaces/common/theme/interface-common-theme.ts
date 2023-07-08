b22fa25ed868337f05e879c7bf0b34097d09cd14
DylanWarrener <dqyqlqaqn@gmail.com>
2023-07-08 17:50:51 +0100

    Themes can store undefined as well

--- src/interfaces/common/theme/interface-common-theme.ts
+++ src/interfaces/common/theme/interface-common-theme.ts
@@ -3,6 +3,6 @@ import { ThemeDefinition } from "vuetify";
 export default interface ICommonThemes extends ThemeDefinition {
 	dark: boolean;
 	colors: {
-		[key: string]: string;
+		[key: string]: string | undefined;
 	};
 }
