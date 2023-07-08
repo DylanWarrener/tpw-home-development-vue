65de9f5ea071ced1a23c195209a19e0276f7b499
DylanWarrener <dqyqlqaqn@gmail.com>
2023-07-08 17:52:11 +0100

    Made app bar denser

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,6 @@
 <template>
 	<!--<v-system-bar color="blue"></v-system-bar>-->
-	<v-app-bar prominent color="primary" scroll-behavior="hide" :height="height">
+	<v-app-bar prominent color="primary" density="compact" scroll-behavior="hide">
 		<template #prepend>
 			<v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 		</template>
@@ -30,53 +30,59 @@ export default defineComponent({
 	},
 	computed: {
 		// Text
+		txtHome(): string {
+			return this.$t("header.appBar.titles.home");
+		},
+		txtKitchen(): string {
+			return this.$t("header.appBar.titles.kitchen");
+		},
+		txtBathroom(): string {
+			return this.$t("header.appBar.titles.bathroom");
+		},
+		txtNewbuild(): string {
+			return this.$t("header.appBar.titles.newbuild");
+		},
+		txtExtension(): string {
+			return this.$t("header.appBar.titles.extension");
+		},
+		txtRefurbishment(): string {
+			return this.$t("header.appBar.titles.refurbishment");
+		},
+		txtContact(): string {
+			return this.$t("header.appBar.titles.contact");
+		},
+		txtAbout(): string {
+			return this.$t("header.appBar.titles.about");
+		},
 		title(): string {
-			const txtHome: string = this.$t("header.appBar.text.home");
-			const txtKitchen: string = this.$t("header.appBar.text.kitchen");
-			const txtBathroom: string = this.$t("header.appBar.text.bathroom");
-			const txtNewbuild: string = this.$t("header.appBar.text.newbuild");
-			const txtExtension: string = this.$t("header.appBar.text.extension");
-			const txtRefurbishment: string = this.$t("header.appBar.text.refurbishment");
-			const txtContact: string = this.$t("header.appBar.text.contact");
-			const txtAbout: string = this.$t("header.appBar.text.about");
 			let retVal: string = "";
 			switch (this.$route.name) {
-				case txtHome:
-					retVal = txtHome;
+				case this.txtHome:
+					retVal = this.txtHome;
 					break;
-				case txtKitchen:
-					retVal = txtKitchen;
+				case this.txtKitchen:
+					retVal = this.txtKitchen;
 					break;
-				case txtBathroom:
-					retVal = txtBathroom;
+				case this.txtBathroom:
+					retVal = this.txtBathroom;
 					break;
-				case txtNewbuild:
-					retVal = txtNewbuild;
+				case this.txtNewbuild:
+					retVal = this.txtNewbuild;
 					break;
-				case txtExtension:
-					retVal = txtExtension;
+				case this.txtExtension:
+					retVal = this.txtExtension;
 					break;
-				case txtRefurbishment:
-					retVal = txtRefurbishment;
+				case this.txtRefurbishment:
+					retVal = this.txtRefurbishment;
 					break;
-				case txtContact:
-					retVal = txtContact;
+				case this.txtContact:
+					retVal = this.txtContact;
 					break;
-				case txtAbout:
-					retVal = txtAbout;
+				case this.txtAbout:
+					retVal = this.txtAbout;
 					break;
 			}
 			return retVal;
-		},
-
-		// Style
-		height: {
-			get(): number {
-				return this.storeCommon.getAppBarHeight;
-			}, 
-			set(newValue: number): void{
-				this.storeCommon.setAppBarHeight(newValue);
-			}
 		},
 
 		// Icons
