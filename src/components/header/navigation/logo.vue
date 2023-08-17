27525196a07cf0d895b1191d23834ddb0cb75cc0
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-08-17 16:07:11 +0100

    Added logo component

---
+++ src/components/header/navigation/logo.vue
@@ -0,0 +1,16 @@
+<template>
+	<img :src="src" style="width: 200px; height: 100%" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "logo-component",
+	props: {
+		src: { type: String, required: true },
+		width: { type: String, required: false, default: "200px" },
+		height: { type: String, required: false, default: "100%" },
+	},
+});
+</script>
