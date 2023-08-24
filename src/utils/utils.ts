d837ad32d5a3761af54f3d28d26c0ef543404c45
DylanWarrener <dqyqlqaqn@gmail.com>
2023-08-24 17:16:54 +0100

    Added a function for selecting an element

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,24 @@
 // Void functions
 export const scrollToElement = (elementsTopBorder: number): void => {
-  window.scrollTo({
-    top: elementsTopBorder,
-    behavior: "smooth",
-  });
+    window.scrollTo({
+        top: elementsTopBorder,
+        behavior: "smooth",
+    });
 };
+
+export const selectingElementToScrollTo = (
+    element: string,
+    eventID: number,
+    pageName: string,
+    btnText: string,
+    id: string,
+    appBarHeight: number
+): void => {
+    const chosenElement: any = document.getElementById(id);
+
+    if (comparableElement === `${eventID}_${pageName.toString()}_${btnText.toLowerCase()}`) {
+        scrollToElement(chosenElement!.offsetTop - appBarHeight);
+    }
+};
+
+// Return functions
