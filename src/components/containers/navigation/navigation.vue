4603af01806d194a1857994e75fca1702629fea5
DylanWarrener <dqyqlqaqn@gmail.com>
2023-08-25 16:00:05 +0100

    Updated common navigation file

--- src/components/containers/navigation/navigation.vue
+++ src/components/containers/navigation/navigation.vue
@@ -1,13 +1,15 @@
 <template>
-	<v-list nav>
-		<v-list-item
-			v-for="item in navigation"
-			:key="item.id"
-			:prepend-icon="item.icon"
-			:title="item.title"
-			:to="item.link"
-		></v-list-item>
-	</v-list>
+    <v-navigation-drawer temporary location="top" v-model="drawer">
+        <v-list nav>
+            <v-list-item
+                :prepend-icon="item.icon"
+                :title="item.title"
+                :to="item.link"
+                :key="index"
+                v-for="(item, index) in navigation"
+            ></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script lang="ts">
@@ -17,67 +19,26 @@ import { defineComponent } from "vue";
 import { parentStore } from "../../../plugins/pinia/pinia";
 
 // Interface
-import { ICommonNavigationData } from "../../../interfaces/header/navigation/interface-header-navigation";
+import { ICommonNavigationItem } from "../../../interfaces/header/navigation/interface-header-navigation";
 
 export default defineComponent({
-	name: "navigation-component",
-	setup() {
-		const storeCommon = parentStore();
-		return { storeCommon };
-	},
-	data(): ICommonNavigationData {
-		return {
-			navigation: [
-				{
-					id: 0,
-					title: this.$t("header.navigation.page.name.home"),
-					icon: this.$t("header.navigation.page.icon.home"),
-					link: this.$t("header.navigation.page.link.home"),
-				},
-				{
-					id: 1,
-					title: this.$t("header.navigation.page.name.kitchen"),
-					icon: this.$t("header.navigation.page.icon.kitchen"),
-					link: this.$t("header.navigation.page.link.kitchen"),
-				},
-				{
-					id: 2,
-					title: this.$t("header.navigation.page.name.bathroom"),
-					icon: this.$t("header.navigation.page.icon.bathroom"),
-					link: this.$t("header.navigation.page.link.bathroom"),
-				},
-				{
-					id: 3,
-					title: this.$t("header.navigation.page.name.newbuild"),
-					icon: this.$t("header.navigation.page.icon.newbuild"),
-					link: this.$t("header.navigation.page.link.newbuild"),
-				},
-				{
-					id: 4,
-					title: this.$t("header.navigation.page.name.extension"),
-					icon: this.$t("header.navigation.page.icon.extension"),
-					link: this.$t("header.navigation.page.link.extension"),
-				},
-				{
-					id: 5,
-					title: this.$t("header.navigation.page.name.refurbishment"),
-					icon: this.$t("header.navigation.page.icon.refurbishment"),
-					link: this.$t("header.navigation.page.link.refurbishment"),
-				},
-				{
-					id: 6,
-					title: this.$t("header.navigation.page.name.contact"),
-					icon: this.$t("header.navigation.page.icon.contact"),
-					link: this.$t("header.navigation.page.link.contact"),
-				},
-				{
-					id: 7,
-					title: this.$t("header.navigation.page.name.about"),
-					icon: this.$t("header.navigation.page.icon.about"),
-					link: this.$t("header.navigation.page.link.about"),
-				},
-			],
-		};
-	},
+    name: "navigation-component",
+    props: {
+        navigation: { type: Array<ICommonNavigationItem>, required: true },
+    },
+    computed: {
+        drawer: {
+            get(): boolean {
+                return this.storeCommon.getDrawer;
+            },
+            set(newValue: boolean) {
+                this.storeCommon.setDrawer(newValue);
+            },
+        },
+    },
+    setup() {
+        const storeCommon = parentStore();
+        return { storeCommon };
+    },
 });
 </script>
