67569d8361cbfc95b3b6c13fdba76c47c556f6bd
DylanWarrener <dqyqlqaqn@gmail.com>
2023-08-25 15:58:11 +0100

    Added news item to textual data interface

--- src/interfaces/common/page/interface-common-page.ts
+++ src/interfaces/common/page/interface-common-page.ts
@@ -1,39 +1,40 @@
 import {
-	IHomeCardTextualData,
-	IKitchenCardTextualData,
-	IBathroomCardTextualData,
-	INewbuildCardTextualData,
-	IExtensionCardTextualData,
-	IRefurbishmentCardTextualData,
-	IContactCardTextualData,
-	IAboutCardTextualData,
+    IHomeCardTextualData,
+    IKitchenCardTextualData,
+    IBathroomCardTextualData,
+    INewbuildCardTextualData,
+    IExtensionCardTextualData,
+    IRefurbishmentCardTextualData,
+    IContactCardTextualData,
+    IAboutCardTextualData,
 } from "../card/interface-common-card";
 
 // Locale textual data
 export default interface ICommonPageTextualData {
-	name: ICommonPageStringTextualData;
-	link?: ICommonPageStringTextualData;
-	icon?: ICommonPageStringTextualData;
+    name: ICommonPageStringTextualData;
+    link?: ICommonPageStringTextualData;
+    icon?: ICommonPageStringTextualData;
 }
 
 export interface ICommonPageStringTextualData {
-	home: string;
-	kitchen: string;
-	bathroom: string;
-	newbuild: string;
-	extension: string;
-	refurbishment: string;
-	contact: string;
-	about: string;
+    home: string;
+    kitchen: string;
+    bathroom: string;
+    newbuild: string;
+    extension: string;
+    refurbishment: string;
+    contact: string;
+    about: string;
+    news: string;
 }
 
 export interface ICommonPageCardTextualData {
-	home: IHomeCardTextualData;
-	kitchen: IKitchenCardTextualData;
-	bathroom: IBathroomCardTextualData;
-	newbuild: INewbuildCardTextualData;
-	extension: IExtensionCardTextualData;
-	refurbishment: IRefurbishmentCardTextualData;
-	contact: IContactCardTextualData;
-	about: IAboutCardTextualData;
+    home: IHomeCardTextualData;
+    kitchen: IKitchenCardTextualData;
+    bathroom: IBathroomCardTextualData;
+    newbuild: INewbuildCardTextualData;
+    extension: IExtensionCardTextualData;
+    refurbishment: IRefurbishmentCardTextualData;
+    contact: IContactCardTextualData;
+    about: IAboutCardTextualData;
 }
