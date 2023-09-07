8ad2bfb91788031175f4587f8ce0aaa156b074ea
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-09-07 17:29:23 +0100

    Updated card titles

--- src/components/containers/card/card.vue
+++ src/components/containers/card/card.vue
@@ -1,32 +1,32 @@
 <template>
-    <v-card class="pa-4 d-flex flex-column bg-background-secondary text-default" min-width="300px">
-        <v-card-item v-if="title || subtitle">
-            <!-- Title -->
-            <v-card-title class="pa-2">
-                <slot name="title">
-                    <h1>{{ title }}</h1>
-                </slot>
-            </v-card-title>
+	<v-card class="pa-4 bg-background-secondary text-default" min-width="300px">
+		<v-card-item class="pa-4" v-if="title || subtitle">
+			<!-- Title -->
+			<v-card-title :tag="titleTag" style="line-height: 72px" class="pa-2 text-h2 text-uppercase text-wrap" :class="titleClass">
+				<slot name="title">
+					{{ title }}
+				</slot>
+			</v-card-title>
 
-            <!-- Subtitle -->
-            <v-card-subtitle class="pa-2">
-                <slot name="subtitle">
-                    <h3>{{ subtitle }}</h3>
-                </slot>
-            </v-card-subtitle>
-        </v-card-item>
+			<!-- Subtitle -->
+			<v-card-subtitle class="pa-2 text-subtitle-1" :class="subtitleClass">
+				<slot name="subtitle">
+					<h3>{{ subtitle }}</h3>
+				</slot>
+			</v-card-subtitle>
+		</v-card-item>
 
-        <!-- Content -->
-        <v-card-text class="pa-0">
-            <slot name="content"></slot>
-        </v-card-text>
+		<!-- Content -->
+		<v-card-text class="pa-0" :class="contentClass">
+			<slot name="content"></slot>
+		</v-card-text>
 
-        <!-- Actions -->
-        <v-card-actions class="pa-2" v-if="btnText">
-            <v-spacer></v-spacer>
-            <v-btn variant="outlined" :text="btnText" @click="scrollTo"></v-btn>
-        </v-card-actions>
-    </v-card>
+		<!-- Actions -->
+		<v-card-actions class="pa-4" v-if="btnText">
+			<v-spacer></v-spacer>
+			<v-btn variant="outlined" :text="btnText" @click="scrollTo"></v-btn>
+		</v-card-actions>
+	</v-card>
 </template>
 
 <script lang="ts">
@@ -42,28 +42,32 @@ import { EventNames } from "@enums/events";
 import { buildEventString } from "@utils/utils";
 
 export default defineComponent({
-    name: "card-component",
-    props: {
-        cardStyle: { type: String, required: false },
-        title: { type: String, required: false },
-        subtitle: { type: String, required: false },
-        btnText: { type: String, required: false },
-    },
-    methods: {
-        scrollTo(event: PointerEvent): void {
-            const eventTarget: HTMLSpanElement = event.target as HTMLSpanElement;
-            const btnText: string = eventTarget.innerText;
-            const eventID: number = EventNames.CARD_BTN_CLICKED;
-            const pageName: any = this.$route.name!;
-            const eventStr: string = buildEventString(eventID, pageName, btnText);
+	name: "card-component",
+	props: {
+		cardStyle: { type: String, required: false },
+		title: { type: String, required: false },
+		subtitle: { type: String, required: false },
+		btnText: { type: String, required: false },
+		titleClass: { type: String, required: false },
+		subtitleClass: { type: String, required: false },
+		contentClass: { type: String, required: false },
+		titleTag: { type: String, required: false, default: "h2" },
+	},
+	methods: {
+		scrollTo(event: PointerEvent): void {
+			const eventTarget: HTMLSpanElement = event.target as HTMLSpanElement;
+			const btnText: string = eventTarget.innerText;
+			const eventID: number = EventNames.CARD_BTN_CLICKED;
+			const pageName: any = this.$route.name!;
+			const eventStr: string = buildEventString(eventID, pageName, btnText);
 
-            this.storeEvent.setEmittedEvent(eventStr);
-        },
-    },
-    setup() {
-        const storeCommon = parentStore();
-        const storeEvent = eventStores.useGlobalEventStore();
-        return { storeCommon, storeEvent };
-    },
+			this.storeEvent.setEmittedEvent(eventStr);
+		},
+	},
+	setup() {
+		const storeCommon = parentStore();
+		const storeEvent = eventStores.useGlobalEventStore();
+		return { storeCommon, storeEvent };
+	},
 });
 </script>
