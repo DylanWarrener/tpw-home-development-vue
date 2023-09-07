2e149c46d66bd9c29a864a7f93987605288718ef
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-09-07 17:31:03 +0100

    Updated titles

--- src/components/containers/section/section.vue
+++ src/components/containers/section/section.vue
@@ -1,24 +1,26 @@
 <template>
-	<div :id="id" class="pa-8 d-flex flex-column text-center text-default">
+	<section style="gap: 16px" class="pa-8 d-flex flex-column text-default text-center" :id="id" :class="containerClass">
 		<!-- Title slot -->
-		<div class="flex-0-0">
+		<div :class="titleClass">
 			<slot name="title">
-				<h1>{{ title }}</h1>
+				<h3 class="text-h3 text-accent">{{ title }}</h3>
 			</slot>
 		</div>
 
 		<!-- Subtitle slot -->
-		<div class="flex-0-0">
+		<div class="w-100 align-self-center" :class="subtitleClass">
 			<slot name="subtitle">
-				<i>{{ subtitle }}</i>
+				<div class="text-subtitle-1">
+					<i>{{ subtitle }}</i>
+				</div>
 			</slot>
 		</div>
 
 		<!-- Content slot -->
-		<div class="d-flex flex-row flex-wrap flex-1-0 justify-center align-center" style="gap: 16px">
+		<div style="gap: 16px" class="border d-flex flex-row flex-wrap justify-center" :class="contentClass">
 			<slot name="content"></slot>
 		</div>
-	</div>
+	</section>
 </template>
 
 <script lang="ts">
@@ -33,6 +35,10 @@ export default defineComponent({
 		id: { type: String, required: false },
 		title: { type: String, required: false },
 		subtitle: { type: String, required: false },
+		titleClass: { type: String, required: false },
+		subtitleClass: { type: String, required: false },
+		contentClass: { type: String, required: false },
+		containerClass: { type: String, required: false },
 	},
 	setup() {
 		const storeCommon = parentStore();
