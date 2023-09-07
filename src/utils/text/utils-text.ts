73adaccb5e3d5c131b72f5b76e196cd26103eb4f
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-09-07 17:32:25 +0100

    Added text for header icon names

--- src/utils/text/utils-text.ts
+++ src/utils/text/utils-text.ts
@@ -6,21 +6,32 @@ export const btnCardText = "Be Inspired?";
 
 // Page
 export const pageNames: ICommonPageStringTextualData = {
-    home: "Home",
-    kitchen: "Kitchens",
-    bathroom: "Bathrooms",
-    newbuild: "Newbuilds",
-    extension: "Extensions",
-    refurbishment: "Refurbishments",
-    contact: "Contact",
-    about: "About",
-    news: "News",
+	home: "Home",
+	kitchen: "Kitchens",
+	bathroom: "Bathrooms",
+	newbuild: "Newbuilds",
+	extension: "Extensions",
+	refurbishment: "Refurbishments",
+	contact: "Contact",
+	about: "About",
+	news: "News",
 };
 
+// Carousel
 export const carouselDropdownItems: string[] = [
-    pageNames.kitchen,
-    pageNames.bathroom,
-    pageNames.newbuild,
-    pageNames.extension,
-    pageNames.refurbishment,
+	pageNames.kitchen,
+	pageNames.bathroom,
+	pageNames.newbuild,
+	pageNames.extension,
+	pageNames.refurbishment,
 ];
+
+// Icon names
+export const headerIconNames = {
+	menu: "menu",
+	newAccount: "newAccount",
+	account: "account",
+	settings: "settings",
+	search: "search",
+	theme: "theme",
+};
