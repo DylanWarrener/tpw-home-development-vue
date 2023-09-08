f4033eeb79c766161cadd962576d5e7f9e4f2502
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-09-08 08:25:05 +0100

    Changed height on carousel

--- src/components/containers/carousel/carousel.vue
+++ src/components/containers/carousel/carousel.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-carousel cycle show-arrows="hover" color="inverted" progress="accent" height="500px">
+	<v-carousel cycle show-arrows="hover" color="inverted" progress="accent" height="42rem">
 		<v-carousel-item cover v-for="img in images" :key="img.id" :src="img.src"></v-carousel-item>
 	</v-carousel>
 </template>
